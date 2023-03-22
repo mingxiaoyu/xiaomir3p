0d93b7df643901c899203a4d4904da95c9b1999e
mingxiaoyu <fengying0347@163.com>
2023-03-22 20:41:20 +0800

    Update diy-part2.sh

--- op/diy-part2.sh
+++ op/diy-part2.sh
@@ -3,6 +3,6 @@
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.31.1/g' package/base-files/files/bin/config_generate
 
-rm -rf package/feeds/luci-app-passwall
+rm -rf package/feeds/luci/luci-app-passwall
 
 svn co https://github.com/immortalwrt/luci/branches/openwrt-21.02/applications/luci-app-passwall package/feeds/luci-app-passwall
