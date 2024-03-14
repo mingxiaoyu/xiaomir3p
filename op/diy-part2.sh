834fa0fdb7a94d3a8911a8d4b6c4bde9d550fc5a
mingxiaoyu <fengying0347@163.com>
2024-03-14 20:44:31 +0800

    Update diy-part2.sh

--- op/diy-part2.sh
+++ op/diy-part2.sh
@@ -3,7 +3,16 @@
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.31.1/g' package/base-files/files/bin/config_generate
 
-rm -rf package/feeds/luci/luci-app-passwall
-rm -rf feeds/luci/applications/luci-app-passwall
+echo "rm"
+rm -rf package/feeds/luci/luci-app-passwall  >/dev/null 2>&1
+rm -rf feeds/luci/applications/luci-app-passwall  >/dev/null 2>&1
 
-git clone --depth=1 -b luci https://github.com/xiaorouji/openwrt-passwall package/luci-app-passwall
+echo "clone"
+mkdir package/community
+
+pushd package/community
+
+git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall
+# git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall2
+
+popd
